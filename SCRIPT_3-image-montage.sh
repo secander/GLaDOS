ea72cbc608c1c300ee7633aa62ec8c33e23573bc
jbencook <jbenjamincook@gmail.com>
2015-03-25 08:16:00 -0500

    script for 3 image montage

---
+++ SCRIPT_3-image-montage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+fn="$1"
+#of=${fn%.*}.png
+of="$2"
+ 
+lf=`ffprobe -show_streams "$fn" 2> /dev/null | awk -F= '/^nb_frames/ { print $2-1 }'`
+rm -f "*$of"
+ffmpeg -i "$fn" -vf "select='eq(n,0)'" -vframes 1 "0_$of" 2> /dev/null
+ffmpeg -i "$fn" -vf "select='eq(n,`expr $lf / 2`)'" -vframes 1 "1_$of" 2> /dev/null
+ffmpeg -i "$fn" -vf "select='eq(n,$lf)'" -vframes 1 "2_$of" 2> /dev/null
+montage -geometry 512x256+0+0 "*_$of" $of
