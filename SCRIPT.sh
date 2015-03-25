7d520cca7f60a61b60edc080e42a07e8db92785f
William Spearman$ <wspearman@gmail.com>
2015-03-24 23:35:01 -0400

    Some comments and a helper bash script

---
+++ SCRIPT.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+fn="$1"
+#of=${fn%.*}.png
+of="$2"
+ 
+lf=`ffprobe -show_streams "$fn" 2> /dev/null | awk -F= '/^nb_frames/ { print $2-1 }'`
+rm -f "$of"
+ffmpeg -i "$fn" -vf "select='eq(n,$lf)'" -vframes 1 "$of" 2> /dev/null
