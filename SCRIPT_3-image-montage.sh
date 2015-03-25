ec35919b47c9e6907d4931981648ea8f6f163a9a
jbencook <ben.cook@hudl.com>
2015-03-25 21:26:15 +0000

    update montage creation

--- SCRIPT_3-image-montage.sh
+++ SCRIPT_3-image-montage.sh
@@ -5,8 +5,12 @@ fn="$1"
 of="$2"
  
 lf=`ffprobe -show_streams "$fn" 2> /dev/null | awk -F= '/^nb_frames/ { print $2-1 }'`
+
 rm -f "*$of"
 ffmpeg -i "$fn" -vf "select='eq(n,5)'" -vframes 1 "0_$of" 2> /dev/null
 ffmpeg -i "$fn" -vf "select='eq(n,`expr $lf / 2`)'" -vframes 1 "1_$of" 2> /dev/null
-ffmpeg -i "$fn" -vf "select='eq(n,`$lf - 5`)'" -vframes 1 "2_$of" 2> /dev/null
+ffmpeg -i "$fn" -vf "select='eq(n,`expr $lf - 5`)'" -vframes 1 "2_$of" 2> /dev/null
 montage -geometry 512x256+0+0 "*_$of" $of
+rm "0_$of"
+rm "1_$of"
+rm "2_$of"
